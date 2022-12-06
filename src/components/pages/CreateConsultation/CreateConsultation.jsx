a329c547226cc7074ce43698305144e09d61493c
Arthur Theodoro | Front End Developer <arthur.theodoro.sva@hotmail.com>
2022-12-05 23:50:59 -0300

    estilo da pagina

--- src/components/pages/CreateConsultation/CreateConsultation.jsx
+++ src/components/pages/CreateConsultation/CreateConsultation.jsx
@@ -37,35 +37,31 @@ function CreateConsultation() {
 
     return (
         <div>
-            <h1>Nova Consulta</h1>
             <div className={styles.container}>
+                <h1>Nova Consulta</h1>
                 <div className={styles.input_control}>
                     <label htmlFor="especialidade">Especialidade</label>
                     <select name="especialidade" onChange={e => setSelectedValue(e.target.value)} >
-                        <option value={0} >Escolha uma opção</option>
-                        // Creates options with database values
+                        <option value={0}>Escolha uma opção</option>
+                        {/* Creates options with database values */}
                         {Specialty.map((item) =>
                             (<option value={item.id} key={item.id}>{item.name}</option>)
                         )}
                     </select>
                 </div>
                 <div className={styles.doctor_control}>
-                //  render the elements based on the selected options
+                    {/* render the elements based on the selected options */}
                     {Doctor.map((item) => {
                         if (item.idEspecialidade != 0 && item.IdEspecialidade == SelectedValue) {
                             return (<>
                                 <h1 key={11}>{`Doutor(a) ${item.name}`}</h1>
-                                <p key={12}>{`Especialidade:${item.Especialidade}`}</p>
+                                <p key={12}>{`Especialidade: ${item.Especialidade}`}</p>
                                 <p key={13}>{`Endereço: ${item.endereco}`}</p></>
                             )
                         }
                     })}
-
-                    <button>Cofirmar</button>
                 </div>
-
-                <div className={styles.input_control}></div>
-
+                <button className={styles.button}>Confirmar</button>
             </div>
         </div>
     )
